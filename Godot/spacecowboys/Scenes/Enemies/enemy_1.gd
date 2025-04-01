0fef335d97ac9fcf4d8d8e9e6a09d0c4eb5c51fd
Luna <153253648+Bro-dep@users.noreply.github.com>
2025-04-01 02:28:45 -0700

    working on the ai

--- Godot/spacecowboys/Scenes/Enemies/enemy_1.gd
+++ Godot/spacecowboys/Scenes/Enemies/enemy_1.gd
@@ -1,16 +1,32 @@
 extends Node2D
-## Enemy SCript
+## Enemy Script
+
+@export_group("Nodes")
+@export var navAgent : NavigationAgent2D
+
+var manager
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	manager = get_parent()
+	player = manager.get_node("Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	setTarget(player)
+	
 	pass
 
+func setTarget(target):
+	navAgent.target_position = target.position
+	pass
 
+func nextPoint(navAgent):
+	var np = navAgent.get_next_path_position()
+	
+	pass
 
 func _on_health_died(entity: Node) -> void:
 	queue_free()
