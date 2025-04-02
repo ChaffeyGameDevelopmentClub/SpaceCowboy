97c20f4876ea67154e20aee8a34b6fb3a6971ece
Luna <153253648+Bro-dep@users.noreply.github.com>
2025-04-01 23:48:46 -0700

    Enemy Follows

--- Godot/spacecowboys/Scenes/Enemies/enemy_1.gd
+++ Godot/spacecowboys/Scenes/Enemies/enemy_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 ## Enemy Script
 
-const SPEED := 50.0
+const SPEED := 25.0
 
 @export_group("Nodes")
 
@@ -18,26 +18,24 @@ var player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	manager = get_parent()
-	#print(manager.name)
 	level = manager.get_parent()
 	player = level.get_node("Player")
-	#print(player.name)
-	#get_tree_string_pretty()
 	
 	# From Docs
 	navAgent.velocity_computed.connect(Callable(_on_velocity_computed))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#setTarget(player)
-	#nextPoint()
+	set_movement_target(player.position)
+	
 	# Do not query when the map has never synchronized and is empty.
 	if NavigationServer2D.map_get_iteration_id(navAgent.get_navigation_map()) == 0:
+		print_debug("Nav Map Not Found")
 		return
 	if navAgent.is_navigation_finished():
+		print_debug("Nav Finished")
 		return
 	
-	set_movement_target(player.position)
 	movement_delta = SPEED * delta
 	var next_path_position: Vector2 = navAgent.get_next_path_position()
 	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_delta
@@ -47,12 +45,15 @@ func _process(delta: float) -> void:
 		_on_velocity_computed(new_velocity)
 	
 	#region debug window
-	print("debug Window")
-	%PlayerPos.text = "Player Pos: " + str(player.position)
-	%NextPoint.text = "Next Point: " + str(next_path_position)
-	%Velocity.text = "Velocity: " + str(new_velocity)
+	print("debug Window\n")
+	var debugWindow = level.get_node("Player/Debug Window")
+	print(level.get_tree_string_pretty())
+	print(level.name)
+	print(debugWindow.name)
+	debugWindow.get_node("PlayerPos").text = "Player Pos: " + str(player.position)
+	debugWindow.get_node("NextPoint").text = "Next Point: " + str(next_path_position)
+	debugWindow.get_node("Velocity").text = "Velocity: " + str(new_velocity)
 	#endregion
-
 func set_movement_target(movement_target: Vector2):
 	navAgent.set_target_position(movement_target)
 
