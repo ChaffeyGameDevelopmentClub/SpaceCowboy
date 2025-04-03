eac7bb4fb552cb344babd4715c0153a2a4df72ba
Luna <153253648+Bro-dep@users.noreply.github.com>
2025-04-03 14:16:59 -0700

    Update SkillTreeInput.gd

--- Godot/spacecowboys/Scenes/Player/SkillTreeInput.gd
+++ Godot/spacecowboys/Scenes/Player/SkillTreeInput.gd
@@ -39,7 +39,7 @@ func _skillpoints_changed():
 	# Display Stat Increases
 	var stat_list_text : Array[String] = []
 	var stats_raw := skilltree.get_all_nodes()
-	var stats := {}
+	var stats := {} # Need to pass this to player?
 	for k in stats_raw:
 		var v : int = stats_raw[k]
 		if v == 0: continue
@@ -58,40 +58,43 @@ func _skillpoints_changed():
 
 # For mouse Input? 
 func _on_map_node_gui_input(event : InputEvent, path : NodePath, node_in_path : int, resource : WorldmapNodeData):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion: # Move tooltip to mouse pos
 		tooltip_root.global_position = event.global_position
 
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			# If can get skill point? Lowers .max_unlock_cost
 			if skilltree.get_node_state(path, node_in_path) <= 0:
 				if skilltree.can_activate(path, node_in_path):
 					tooltip_root.hide()
 					skilltree.max_unlock_cost -= skilltree.set_node_state(path, node_in_path, 1)
 					_skillpoints_changed()
-
+			# Deactivate Skill?
 			elif skilltree.can_deactivate(path, node_in_path):
 				tooltip_root.hide()
 				skilltree.max_unlock_cost -= skilltree.set_node_state(path, node_in_path, 0)
 				_skillpoints_changed()
+		# Remove Skill Node, Don't need this.
+		#if event.button_index == MOUSE_BUTTON_MIDDLE && event.pressed:
+			#skilltree.get_node(path).remove_node(node_in_path)
 
-		if event.button_index == MOUSE_BUTTON_MIDDLE && event.pressed:
-			skilltree.get_node(path).remove_node(node_in_path)
-
-# Mouse Moves on tool tip?
+# Mouse Moves on tool tip? Displays Name and Desc
 func _on_map_node_mouse_entered(_path : NodePath, _node_in_path : int, resource : WorldmapNodeData):
 	tooltip_root.show()
 	tooltip_title.text = resource.name
 	tooltip_desc.text = resource.desc
 	tooltip_root.size = Vector2.ZERO
-# Mouse moves away from tooltip?
+# Mouse moves away from tooltip? Hides Tooltip
 func _on_map_node_mouse_exited(_path : NodePath, _node_in_path : int, _resource : WorldmapNodeData):
 	tooltip_root.hide()
+
 # Reset Skills
 func _on_reset_skills_pressed():
 	skilltree.max_unlock_cost = starting_skillpoints
 	skilltree.reset()
 	_skillpoints_changed()
-# Use SKill Point?
+
+# Debug to add points
 func _on_add_pressed() -> void:
 	skilltree.max_unlock_cost += 1
 	_skillpoints_changed()
