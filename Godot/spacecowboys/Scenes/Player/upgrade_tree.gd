b26b6bd6a54115d8b28bc3adb785b3e6dd9175c6
The Bron Jame Offical <91028605+TheBronJameOffical@users.noreply.github.com>
2025-04-03 02:45:33 -0700

    mewhen

--- Godot/spacecowboys/Scenes/Player/upgrade_tree.gd
+++ Godot/spacecowboys/Scenes/Player/upgrade_tree.gd
@@ -1,5 +1,16 @@
 extends Control
 
+@export var UpgradeMenu : Control
+
+var menu_up = false
+
 func _input(event): 
 	if Input.is_action_just_pressed("Upgrade"):
-		pass
+		if menu_up:
+			UpgradeMenu.hide()
+			get_tree().paused = false
+			menu_up = false
+		else:
+			UpgradeMenu.show()
+			get_tree().paused = true
+			menu_up = true
